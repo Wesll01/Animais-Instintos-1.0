694a3743c3cf89ba1dc285ddf760f6c143b5cc98
Lucas <lucasgrcayres@gmail.com>
2021-10-10 01:25:05 -0300

    Mudança na concateação

---
+++ README.md
@@ -0,0 +1,17 @@
+# Projeto DevOps (FACULDADE)
+
+Site informando alguns animais em extinção e algumas ONGs de animais em extinção
+
+
+
+### GitHub dos Integrantes: 
+
+##### - Lucas Gabriel Reis Cayres:
+
+https://github.com/ReiVipX
+
+
+
+##### - José Italo
+
+https://github.com/Jose2300
--- main.js
+++ main.js
@@ -1,94 +1,94 @@
-function initTabNav(){
+function initTabNav() {
 
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
-tabContent[0].classList.add('ativo');
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
+  tabContent[0].classList.add('ativo');
 
-if(tabContent.length && tabMenu.length){
+  if (tabContent.length && tabMenu.length) {
 
-function activeTab(index){
-  tabContent.forEach((section)=>{
-    section.classList.remove('ativo');
-  });
-  tabContent[index].classList.add('ativo');
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+      });
+      tabContent[index].classList.add('ativo');
 
-}
-tabMenu.forEach((itemMenu,index)=>{
-  itemMenu.addEventListener('click',()=>{
+    }
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
 
-  activeTab(index);
-  });
+        activeTab(index);
+      });
 
-});
-}
+    });
+  }
 }
 initTabNav();
-function initAccordion(){
+function initAccordion() {
 
-const accordionList = document.querySelectorAll('.js-accordion dt');
-const activeClass = 'ativo';
-if(accordionList.length){
-accordionList[0].classList.add(activeClass);
-accordionList[1].nextElementSibling.classList.add(activeClass);
-function actibeAccordion(){
-  this.classList.toggle('ativo');
-  this.nextElementSibling.classList.toggle('ativo');
-}
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClass = 'ativo';
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[1].nextElementSibling.classList.add(activeClass);
+    function actibeAccordion() {
+      this.classList.toggle('ativo');
+      this.nextElementSibling.classList.toggle('ativo');
+    }
 
-accordionList.forEach((item)=>{
-  item.addEventListener('click',actibeAccordion);
+    accordionList.forEach((item) => {
+      item.addEventListener('click', actibeAccordion);
 
-})
+    })
 
-}
+  }
 }
 initAccordion();
-function initScrollSuave(){
-const linksInterno = document.querySelectorAll('.js-menu a[href^="#"}');
+function initScrollSuave() {
+  const linksInterno = document.querySelectorAll('.js-menu a[href^="#"}');
 
-function scrolltoSection(event){
-  event.preventDefault();
-  const href= event.currentTarget.getAtributte('href');
-  const section = document.querySelector(href);
-  section.scrollIntoView({
-    behavior:'smooth',
-    block:'start',
+  function scrolltoSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAtributte('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    }
+
+    )
+
   }
-
-  )
-
-}
 }
 
 initScrollSuave();
 
 
 
-  const  sections = document.querySelectorAll('.js-scroll');
+const sections = document.querySelectorAll('.js-scroll');
 
-  if(sections.length){
+if (sections.length) {
 
-  const windowMetade = window.innerHeight*0.6;
+  const windowMetade = window.innerHeight * 0.6;
 
-  function animaScroll(){
+  function animaScroll() {
 
-   sections.forEach((section)=>{
-     const sectionTop = section.getBoundingClientRect().top;
-     const isSectionVisible= (sectionTop - windowMetade < 0);
-     if(isSectionVisible){
-       
-       section.classList.add('ativo');
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = (sectionTop - windowMetade < 0);
+      if (isSectionVisible) {
+
+        section.classList.add('ativo');
 
 
-     }else{
-       section.classList.remove('ativo');
+      } else {
+        section.classList.remove('ativo');
 
-     }
-   })
-
-  }
-animaScroll();
-
-window.addEventListener('scroll',animaScroll)
+      }
+    })
 
   }
+  animaScroll();
+
+  window.addEventListener('scroll', animaScroll)
+
+}
